c4aaa691cf03ecc8cb95768288aa0785a66e6282
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-09-25 18:18:57 +0530

    update

--- Seat Booking page/seat_booking.php
+++ Seat Booking page/seat_booking.php
@@ -1,3 +1,7 @@
+<?php
+     include("database.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,10 +45,11 @@
             
             <div id="General Passenger" class="tabcontent">
                 
-                <form>
+                <form method="POST">
+                    
                     <div class="form-group">
                         <label for="nic">NIC</label>
-                        <input type="text" id="nic" name="nic">
+                        <input style="width:100%;" type="text" id="nic" name="nic">
                     </div>
 
                     <div class="form-group">
@@ -64,47 +69,53 @@
 
                     <div class="form-group">
                         <label for="date">Date</label>
-                        <input type="date" id="date" value="2024-09-15">
+                        <input type="date" id="date" value="2024-09-15" name="date" style="width:300px;">
                     </div>
                     
                     <div class="actions">
-                        <button onclick="location.href='../Payment_page/payment.html'" type="button" class="confirm">Confirm</button>
+                        <button type="submit" class="confirm">Confirm</button>
                         <button onclick="location.href='../homepage/home.html'" type="button" class="back">Back</button>
                     </div>
+
+                    <?php
+                        include("functions_general.php");
+                    ?>
+
                 </form>
             </div>
 
             <div id="Warrent Passenger" class="tabcontent">
-                <form>
+                <form method="POST">
                     
                     <div class="form-group">
                         <label for="id">Warrent ID</label>
-                        <input type="text" id="id" name="id">
+                        <input type="text" id="id" name="wid">
                     </div>
                     <div class="form-group">
                         <label for="from">From</label>
-                        <input type="text" id="from" name="from">
+                        <input type="text" id="from" name="wfrom">
                     </div>
                     <div class="form-group">
                         <label for="to">To</label>
-                        <input type="text" id="to" name="to">
+                        <input type="text" id="to" name="wto">
                     </div>
 
                     <div class="form-group">
                         <label for="passengers">No of passengers</label>
-                        <input type="text" id="passengers" name="passengers">
+                        <input type="text" id="passengers" name="wpassengers">
                     </div>
 
                     <div class="form-group">
                         <label for="date">Date</label>
-                        <input type="date" id="date" value="2024-09-15" style="width:300px;">
+                        <input type="date" id="date" value="2024-09-15" name="wdate" style="width:300px;">
                     </div>
                     
-                    
-
+                    <?php
+                        include("functions_warrent.php");
+                    ?>
                     
                     <div class="actions">
-                        <button onclick="location.href='../Payment_page/payment.html'" type="button" class="confirm">Confirm</button>
+                        <button type="submit" class="confirm">Confirm</button>
                         <button onclick="location.href='../homepage/home.html'" type="button" class="back">Back</button>
                     </div>
                 </form>
