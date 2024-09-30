51afd421f8cabc8e4236fa8c08b7bca86519df77
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-09-30 16:23:17 +0530

    update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -13,7 +13,7 @@
     <h3>Seat Booking History<h3>
     <hr><br>
     <div class="button1">
-        <button type="button" class="button" onclick="showDetails()">Details</button>
+        <button type="button" class="button">Details</button>
         <button type="button" class="button">Update</button>
         <button type="button" class="button">Delete</button>
     </div>
