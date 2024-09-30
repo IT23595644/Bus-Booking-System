a426f9a2e1671ce543ea0b314a0f9f53f8fdeab5
Wasika Anusanga <wasikaanusanga12@gmail.com>
2024-09-30 17:15:25 +0530

    cusSup

---
+++ customersupporter/customerview.php
@@ -0,0 +1,32 @@
+<?php
+    include("../Headers-Footers/header.php");
+?>
+
+    <link rel="stylesheet" href="../customersupporter/crud.css"> 
+
+    <div class="title">
+        <h1>Manage Customer Supporters</h1>
+    </div>
+    
+
+    <div class="box">      
+        <div class="button1">
+            
+            <button type="button" class="button">Add Supporter</button><br>
+            <?php
+            
+            
+
+            
+            ?>
+
+        </div>
+    </div>
+
+    
+
+    <br>
+    
+<?php
+    include("../Headers-Footers/footer.php");
+?>
