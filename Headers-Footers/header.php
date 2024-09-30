3ffda262ec177e3f186d43fb714c91fb2a40ca4e
Wasika Anusanga <wasikaanusanga12@gmail.com>
2024-09-30 14:56:40 +0530

    userDashboard

--- Headers-Footers/header.php
+++ Headers-Footers/header.php
@@ -25,6 +25,9 @@
                     align-items: center;
                     text-align: center;
                 }
+                .iconlogo{
+                    font-size: 4rem;
+                }
                 .nav{
                     display:flex;
                     justify-content: space-between;
@@ -86,7 +89,8 @@
         <div class="mainContainer"
         <header>
                 <div class="contain">
-                    <img src="./Headers-Footers/logo.png" width="8%">
+                <!--<img src="./Headers-Footers/logo.png" width="8%">-->
+                    <div class="iconlogo">🚌</div>
                     <div>
                         <h1>Bus365</h1>
                         <h6>Ride Easy, Ride with Bus365</h6>
