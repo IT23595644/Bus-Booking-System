d451fb5d88fa9095cde103ab7e04f2e10685f359
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-09-30 16:22:28 +0530

    update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -3,6 +3,7 @@
 ?>
 
 <link rel="stylesheet" href="Admin dashboard/admin_styles.css"> 
+<script src="Admin dashboard/admin.js"></script>
 
 <div class="title">
     <h1>Admin Dashboard</h1>
@@ -12,7 +13,7 @@
     <h3>Seat Booking History<h3>
     <hr><br>
     <div class="button1">
-        <button type="button" class="button">Details</button>
+        <button type="button" class="button" onclick="showDetails()">Details</button>
         <button type="button" class="button">Update</button>
         <button type="button" class="button">Delete</button>
     </div>
