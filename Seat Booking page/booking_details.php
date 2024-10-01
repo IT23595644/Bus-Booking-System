7f2e592c11a220444373127c058c523d60b7b50e
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-10-02 01:36:48 +0530

    update

---
+++ Seat Booking page/booking_details.php
@@ -0,0 +1,42 @@
+<?php
+    include_once("../config.php");
+    include("../Headers-Footers/header.php");
+?>
+
+    <link rel="stylesheet" href="./booking_crud.css"> 
+    
+
+    <div class="title">
+        <h1>Seat Booking Details</h1>
+    </div>
+    
+
+    <div class="box">      
+            
+            <a href="../seat_booking.php" class="button">Book another seat</a>
+
+            <div>
+                <table>
+                <tr>
+                    <th>Booking_ID</th><th>Seat_no</th><th>Date</th><th>Bus_ID</th><th>Location</th><th>Destination</th><th>No_of_passengers</th><th colspan="2">Operation</th>
+                </tr>
+                <?php
+                $sql="SELECT * FROM seatbooks;";
+                
+                $result=mysqli_query($conn,$sql);
+
+                if(mysqli_num_rows($result)>0)
+                {    
+                        while($row=mysqli_fetch_assoc($result))
+                        {
+                            echo "<tr><td>".$row['bookingId']."<td>". $row['seatNum']."</td><td>".$row['date']."</td><td>".$row['busId']."</td><td>".$row['Location']."</td><td>".$row['Destination']."</td><td>".$row['Passengers']."</td><td><button class='update-btn'><a href='./booking_update.php?updateid=".$row['bookingId']."'>UPDATE</a></button></td><td><button class='delete-btn'><a href='booking_delete.php?deleteid=".$row['bookingId']."'>DELETE</a></button></td></tr>";
+                        }
+                }
+                ?>
+                </table>
+            </div>
+    </div>
+    
+<?php
+    include("../Headers-Footers/footer.php");
+?>
---
+++ Seat Booking page/booking_update.php
@@ -0,0 +1,91 @@
+<?php 
+    include ('../config.php');
+        
+        if(isset($_GET['updateid'])){   
+            $id=$_GET['updateid'];
+        $sql="SELECT * FROM seatbooks where bookingId=$id";
+        $result=mysqli_query($conn,$sql);
+        $row=mysqli_fetch_assoc($result);
+
+        $seatNum=$row['seatNum'];
+        $date=$row['date'];
+        $busId=$row['busId'];
+        $location=$row['Location'];
+        $destination=$row['Destination'];
+        $passengers=$row['Passengers'];
+        
+        }   
+        
+        if($_SERVER["REQUEST_METHOD"]=="POST")
+        {   
+            
+            $bookFrom=$_POST["from"];
+            $bookTo=$_POST["to"];
+            $bookPassengers=$_POST["passengers"];
+            $bookSeatnum=$_POST["seatnum"];
+            $bookBusid=$_POST["busid"];
+            echo $bookFrom ;
+            $sql="UPDATE seatbooks  SET Location='$bookFrom',Destination='$bookTo',Passengers='$bookPassengers',seatNum='$bookSeatnum',busId='$bookBusid' WHERE bookingId='$id';";
+
+            
+            $result=mysqli_query($conn,$sql);
+            if($result)
+            {
+                header("location: booking_details.php");
+            }
+
+            mysqli_close($conn);
+
+        }
+
+?>
+
+
+<?php
+    include("../Headers-Footers/header.php");
+?>
+
+    <link rel="stylesheet" href="booking_crud.css"> 
+
+    <div class="title">
+        <h1>Update Seat Booking Details</h1>
+    </div>
+    
+
+    <div class="box_update">      
+        
+        <div class="form">
+            <form method="post" style="gap:20px;" >
+                
+                <label>From:</label><br>
+                <input type="text" name="from" ><br>
+
+                <label>To:</label><br>
+                <input type="text" name="to" ><br>
+
+                <label>No of passengers:</label><br>
+                <input type="text" name="passengers" ><br>
+
+                <label>Seat No:</label><br>
+                <input type="text" name="seatnum" ><br>
+
+                <label>Bus ID:</label><br>
+                <input type="text" name="busid" ><br><br>
+
+                <input class="button_update" type="submit" name="submit" value="Update">               
+
+            </form>
+        </div>
+
+    </div>
+
+    
+
+    <br>
+    
+<?php
+    include("../Headers-Footers/footer.php");
+?>
+
+
+
