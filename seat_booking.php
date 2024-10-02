353149274e19b0289edb12c20aef7b6908c54b94
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-10-02 10:34:35 +0530

    update

--- seat_booking.php
+++ seat_booking.php
@@ -17,7 +17,94 @@
     
     <div class="main-section">
         <div class="image-section">
-            <img src="Seat Booking page/e.png" alt="bus_art" height="350px" width="375px">
+          
+            <style>
+            .mySlides {
+                display: none;
+            }
+            img {
+                vertical-align: middle;
+            }
+
+            /* Slideshow container */
+            .slideshow-container {
+            max-width: 1000px;
+            position: relative;
+            margin: auto;
+            }
+
+            .dot {
+                height: 15px;
+                width: 15px;
+                margin: 0 2px;
+                background-color: #bbb;
+                border-radius: 50%;
+                display: inline-block;
+                transition: background-color 0.6s ease;
+            }
+            .active {
+                background-color: white;
+            }
+
+            /* Fading animation */
+            .fade {
+            animation-name: fade;
+            animation-duration: 3.5s;
+            }
+
+            @keyframes fade {
+            from {opacity: .4} 
+            to {opacity: 1}
+            }
+            </style>
+            
+            <div class="slideshow-container">
+
+                <div class="mySlides fade">
+                    <img src="Seat Booking page/a.jpeg" style="width:100%">
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="Seat Booking page/d.jpg" style="width:100%">
+    
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="Seat Booking page/e.png" style="width:100%">
+                </div>
+
+                <div style="text-align:center">
+                    <span class="dot"></span> 
+                    <span class="dot"></span> 
+                    <span class="dot"></span> 
+                </div>
+
+            </div>
+            
+            <script>
+                let slideIndex = 0;
+                showSlides();
+
+                function showSlides() {
+                let i;
+                let slides = document.getElementsByClassName("mySlides");
+                let dots = document.getElementsByClassName("dot");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";  
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {slideIndex = 1}    
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex-1].style.display = "block";  
+                dots[slideIndex-1].className += " active";
+                setTimeout(showSlides, 5000); // Change image every 2 seconds
+                }
+            </script>
+
+
+        
         </div>
 
         <div class="booking-form">
