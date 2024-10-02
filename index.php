706d40cd12d40b93cf51a60fc05dae6efe22a4ac
Wasika Anusanga <wasikaanusanga12@gmail.com>
2024-10-02 21:46:51 +0530

    Index and Css Created

---
+++ index.php
@@ -0,0 +1,226 @@
+<?php
+        include_once("Headers-Footers/header.php");
+  ?>
+  <style>
+    .fooot {
+    background-color: #444;
+    color: #fff;
+    text-align: center;
+    padding: 2rem 0;
+}
+
+.fooot nav {
+    margin-bottom: 1rem;
+}
+
+.fooot nav a {
+    color: #fff;
+    text-decoration: none;
+    margin: 0 0.5rem;
+}
+.fooot nav a {
+    color: #fff;
+    text-decoration: none;
+    margin: 0 0.5rem;
+}
+
+.fooot .social-media a {
+    color: #fff;
+    margin: 0 1rem;
+    text-decoration: none;
+}
+
+.fooot p {
+    margin-top: 1rem;
+    font-size: 0.8rem;
+}
+#link-a{
+    text-decoration: none;
+}
+#link-a:visited{
+    color:#fff;
+}
+
+  </style>
+    <link rel="stylesheet" href="../Bus-Booking-System/homepage/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="icon" href="../Bus-Booking-System/Headers-Footers/logo.png" type="image/png">
+
+<div class="content">
+    <div class="location">
+        <div class="locat">
+            <p>Travel To</p>
+            <div class="places">
+                <span>Matara</span>
+                <span>Kataragama</span>
+                <span>Colombo</span>
+                <span>Trincomalee</span>
+                <span>Matara</span>
+            </div>
+        </div>
+    </div>
+
+    <section>
+        <div class="welc">
+<<<<<<< HEAD
+            <h1>WELCOME</h1>
+=======
+            <h1>WELCOME TO <strong>BUS365</strong></h1>
+>>>>>>> 89c0123e191b43ae44020ddb6c68ce5d77284ef4
+            <h3>Your Safe Travel Our Main Goal</h3>
+            <h3>Ride Easy, Ride with Bus365.</h3><br>
+           <center><a href="seat_booking.php"class="book-btn">Book Ticket</a></center>
+        </div>
+    </section>
+</div>
+<div class="testimonials">
+  <div class="inner">
+    <h1>Popular Route</h1>
+    <div class="border"></div>
+    <div class="card">
+        <div class="Route-card">
+            <img src="homepage/images/new-2.jpg">
+            <div class="card-cont">
+                <h1>ND - 2256 </h1>
+                <p>Matara to Katharagama</p>
+                <a href="#" class="card-btn">read more</a>
+            </div>
+        </div>
+        <div class="Route-card">
+            <img src="../Bus-Booking-System/homepage/images/xx.png">
+            <div class="card-cont">
+                <h1>NC - 0056 </h1>
+                <p>Badulla to Ampara</p>
+                <a href="#" class="card-btn">read more</a>
+            </div>
+        </div>
+        <div class="Route-card">
+            <img src="../Bus-Booking-System/homepage/images/bus1.jpg">
+            <div class="card-cont">
+                <h1>NB - 1089 </h1>
+                <p>Tangalle to Jaffna</p>
+                <a href="#" class="card-btn">read more</a>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<div class="testimonials">
+    <div class="inner">
+      <h1>Why Book with BusSeat?</h1>
+      <div class="border"></div>
+
+      <div class="row">
+        <div class="col">
+                    <ul>
+                        <li><div class="icon">🔍</div>More Choices</li>
+                        <li><div class="icon">🙂‍↔️</div>Customer Support</li>
+                        <li><div class="icon">🎫</div>Best Price</li>
+                        <li><div class="icon">🗺️</div>Google Map Location</li>
+                    </ul>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="testimonials">
+    <div class="inner">
+      <h1>User Reviews</h1>
+      <div class="border"></div>
+
+      <div class="row">
+        <div class="col">
+          <div class="testimonial">
+            <img src="../Bus-Booking-System/homepage/images/p1.png" alt="">
+            <div class="name">Wasika</div>
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+
+            <p>
+            <h3>Best Passenger Service</h3>
+            Overall good experience, and Good passenger service and buses have confortable seats. 
+<br>
+<h6>Date of experience: April 12, 2024</h6>
+            </p>
+            <a href="userRating.php" class="card-btn">read more</a>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="testimonial">
+            <img src="../Bus-Booking-System/homepage/images/p2.png" alt="">
+            <div class="name">Nethmi</div>
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+
+            <p>
+            <h3>User friendly Bus Ticket Website</h3>
+It was easy to purchase bus tickets on Bus365.lk website. 
+Some of the companies do not have admin charge, which is good. Keep it up guys!
+<br>
+<h6>Date of experience: August 20, 2024</h6>
+            </p>
+            <a href="userRating.php" class="card-btn">read more</a>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="testimonial">
+            <img src="../Bus-Booking-System/homepage/images/p3.png" alt="">
+            <div class="name">Supuni</div>
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+
+            <p>
+          <h3>  Overall good experience </h3>
+
+            Overall good experience, but can improve on more options for cancel booking and refund
+            <br>
+            <h6>Date of experience: October 30, 2023</h6>
+            </p>
+            <a href="userRating.php" class="card-btn">read more</a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class=fooot>
+      <nav class="footer-nav">
+          <a href="#">Home</a> | 
+          <a href="#">Scheduling</a> | 
+          <a href="#">Booking</a> | 
+          <a href="#">About</a> | 
+          <a href="#">Contact</a>
+      </nav>
+      <div class="social-media">
+          <a href="#"><i class="ri-facebook-fill"></i></a>
+          <a href="#"><i class="ri-linkedin-fill"></i></a>
+          <a href="#"><i class="ri-google-fill"></i></a>
+      </div>
+      <p>Copyright © 2024 Website. All rights reserved.</p>
+      <p><a href="#"id="link-a">Terms & Conditions</a> | <a href="#"id="link-a">Privacy Policy</a></p>
+  </div>
+  <button id="back-to-top" onclick="scrollToTops()"><i class="ri-arrow-up-fill"></i></button>
+    <script src="home.js"></script>
+
+  <?php
+        include_once("../Bus-Booking-System/Headers-Footers/footer.php");
+  ?>
+  
