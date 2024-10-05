0fa0da46892656186be68099419f7d67ba80d5d6
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-10-05 23:10:42 +0530

    update

--- seat_booking_reciept.php
+++ seat_booking_reciept.php
@@ -10,113 +10,15 @@
 <div class="body1234">
     <br><br>
     <div class="title">
-        <h1>Book Your Seats</h1>
+        <h1>Payment Receipt</h1>
     </div>
     
     <div class="main-section">
-        <div class="image-section">
-
-            <style>
-                .mySlides {
-                    display: none;
-                }
-                img {
-                    vertical-align: middle;
-                }
-
-                .slideshow-container {
-                    max-width: 1000px;
-                    position: relative;
-                    margin: auto;
-                }
-
-                .dot {
-                    height: 15px;
-                    width: 15px;
-                    margin: 0 2px;
-                    background-color: grey;
-                    border-radius: 50%;
-                    display: inline-block;
-                    transition: background-color 0.6s ease;
-                }
-                .active {
-                    background-color: white;
-                }
-                
-                .fade {
-                    animation-name: fade;
-                    animation-duration: 3.5s;
-                }
-
-                @keyframes fade {
-                    from {opacity: .4} 
-                    to {opacity: 1}
-                }
-                img{
-
-                }
-            </style>
-            
-             <div class="slideshow-container">
-
-                <div class="mySlides fade">
-                    <img src="Seat Booking page/slideshow1.png" style="width:350px; height:320px;">
-                </div>
-
-                <div class="mySlides fade">
-                    <img src="Seat Booking page/slideshow2.png" style="width:350px; height:320px;">
-    
-                </div>
-
-                <div class="mySlides fade">
-                    <img src="Seat Booking page/slideshow3.png" style="width:350px; height:320px;">
-                </div>
-
-                <div style="text-align:center">
-                    <span class="dot"></span> 
-                    <span class="dot"></span> 
-                    <span class="dot"></span> 
-                </div>
-
-            </div>
-            
-            <script>
-                let slideIndex = 0;
-                showSlides();
-
-                function showSlides() {
-                let i;
-                let slides = document.getElementsByClassName("mySlides");
-                let dots = document.getElementsByClassName("dot");
-                
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";  
-                }
-
-                slideIndex++;
-                if (slideIndex > slides.length) {
-                    slideIndex = 1
-                }
-
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-
-                slides[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " active";
-                setTimeout(showSlides, 5000); 
-                }
-
-            </script>
-
-
         
-        </div>
-
-        <div class="booking-form">
+            <div class="booking-form">
            
             <div class="tabs">
-                <div class="tab active">Booking Details</div>
+                <div class="tab active">Ticket Details</div>
             </div>
             
             <div id="Customer Details" class="tabcontent">
@@ -183,13 +85,7 @@
                         <button onclick="location.href='index.php'" type="button" class="back">Back</button>
                     </div>
                     
-                    
-                    
-
-                    
-            </div>
-
-                
+                </div>
             </div>
         </div>
     </div>
