edeac99d9af5b7ce678882d869279b60ba99138b
savin69 <saween910@gmail.com>
2024-10-06 01:09:47 +0530

    About us

---
+++ About US/about US.php
@@ -0,0 +1,32 @@
+<?php
+        include_once("Headers-Footers/header.php");
+  ?>
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="styles.css">
+
+<div class="sec">
+    <div class="cont">
+        <div class="cont-sec">
+            <div class="title">
+                <h1>About Us</h1>
+            </div>
+            <div class="content">
+                <h3>Techkitez (Pvt) Ltd developed "BusSeat.lk" - an innovative online bus ticket booking platform, 
+                    envisioning a hassle-free and enhanced public transportation system in Sri Lanka. Since our inception in 2015, we have managed to effectively capture a plethora of returning
+                <p>Public transport, specifically travel by bus is a large growing business in Sri Lanka and other countries; 
+                    hence innovatory mechanisms towards bus ticket booking systems is imperative to adequately provide a user-friendly interface for the passenger to reserve a seat on a journey, and an effective compilation and maintenance of individual records for bus companies and regulatory bodies.
+                </p>
+                <br>
+                <p>
+                An efficient transport system is the key for the betterment of a country. Although the transport system in Sri Lanka has seen many improvements over time, the bus ticket booking platform still lacks a significant technology advantage. Pens and papers are unable to cope with the massive booking requests from passengers all over the Island. 
+                Since our inception, we have seen that the general public and tourists alike are not completely aware of bus timetables and how to find a bus, and where to find it. After using our platform, Around 100,000 people from all parts of the country have given us very positive feedback that the online bus ticket booking platform has improved their journey with efficiency and convenience, 
+                two pertinent and fundamental factors necessary to improve Sri Lanka's transport system.
+                </p>
+                </h3>
+            </div>
+            <div class="social">
+
+            </div>
+        </div>
+    </div>
+</div>
