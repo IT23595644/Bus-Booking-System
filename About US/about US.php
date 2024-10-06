94add069bb6b3891ca66207a101fccd8fb6dbf68
savin69 <saween910@gmail.com>
2024-10-06 12:57:29 +0530

    contacat us page update

--- About US/about US.php
+++ About US/about US.php
@@ -11,20 +11,23 @@
                 <h1>About Us</h1>
             </div>
             <div class="content">
-                <h3>Techkitez (Pvt) Ltd developed "BusSeat.lk" - an innovative online bus ticket booking platform, 
-                    envisioning a hassle-free and enhanced public transportation system in Sri Lanka. Since our inception in 2015, we have managed to effectively capture a plethora of returning
-                <p>Public transport, specifically travel by bus is a large growing business in Sri Lanka and other countries; 
-                    hence innovatory mechanisms towards bus ticket booking systems is imperative to adequately provide a user-friendly interface for the passenger to reserve a seat on a journey, and an effective compilation and maintenance of individual records for bus companies and regulatory bodies.
+                <h3>Welcome to Bus365, your one-stop destination for convenient and reliable bus ticket bookings.</h3>
+                <p> We are dedicated to simplifying travel for passengers by offering a fast, secure, and user-friendly platform to book bus tickets across various routes. Our aim is to make bus travel accessible, affordable,
+                     and comfortable for everyone, whether you’re traveling for business or leisure.
+                </p>
+                <p>
+                Our mission is to transform the way people travel by bus, making the booking process hassle-free and offering a high standard of service every step of the way. 
+                We are constantly innovating and expanding our network to better serve our passengers.
                 </p>
            
-                </h3>
+           
             </div>
             <div class="social">
             <a href=""><i class="ri-facebook-circle-fill"></i></a>
             <a href=""><i class="ri-mail-fill"></i></a>
             <a href=""><i class="ri-instagram-line"></i></a>
-
             </div>
+            <br>
         </div>
         <div class="image-section">
             <img src="about us.jpg">
