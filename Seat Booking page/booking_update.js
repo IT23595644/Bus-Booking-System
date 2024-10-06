936643ec6b17f9aa603f3355301d4993a30298cd
SadeepaMethmal <sadeepemethmal@gmail.com>
2024-10-06 16:21:12 +0530

    update

---
+++ Seat Booking page/booking_update.js
@@ -0,0 +1,44 @@
+document.getElementById('form').addEventListener('submit', function(event){
+    //prevent auto submitting
+    var from=document.getElementById('from').value;
+    var to=document.getElementById('to').value;
+    var d_date=document.getElementById('d_date').value;
+    var seatnum=document.getElementById('seatnum').value;
+    var busid=document.getElementById('busid').value;
+
+    //validate the location 
+    if(from.trim()==''){
+        document.getElementById('location_error').innerHTML="Location is required";
+        document.getElementById('location_error').style="color:red";
+        event.preventDefault(); //prevent from submitting the form
+        return;
+    }
+    //validate destination
+    if(to.trim()==''){
+        document.getElementById('destination_error').innerHTML="Destination is required";
+        document.getElementById('destination_error').style="color:red";
+        event.preventDefault();
+        return;
+    }
+    //validate departure_date
+    if(d_date.trim()==''){
+        document.getElementById('d_date_error').innerHTML="Departure date is required";
+        document.getElementById('d_date_error').style="color:red";
+        event.preventDefault();
+        return;
+    }
+    //validate seatnum
+    if(seatnum.trim()==''){
+        document.getElementById('seatnum_error').innerHTML="Seat_No is required";
+        document.getElementById('seatnum_error').style="color:red";
+        event.preventDefault();
+        return;
+    }
+    //validate busid
+    if(busid.trim()==''){
+        document.getElementById('busid_error').innerHTML="Bus_id is required";
+        document.getElementById('seatnum_error').style="color:red";
+        event.preventDefault();
+        return;
+    }
+})
